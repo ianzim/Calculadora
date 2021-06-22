421b2a23c5cf3ce7168df0f379f92f3e7462531d
Ian Figueiredo <iandlf2347@gmail.com>
2021-06-22 17:14:27 -0300

    Adicionado o começo da função de calcular integrais

--- calculus.py
+++ calculus.py
@@ -1,2 +1,9 @@
 import math
 
+def integral():
+    a = float(input("Ponto inicial da integração: "))
+    b = float(input("Ponto final da integração: "))
+    func = str(input("Função a ser integrada: "))
+
+def derivada():
+    pass
